827e031c205621d8e5377e5a28b58e07a0640c7a
Gervin Luide <gervin.luide@khk.ee>
2023-01-31 11:23:29 +0200

    Created form request responce with API data

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.set('views', path.join(__dirname, 'views'))
 const key = 'acf35c2c24deeb6a70c7dbffd5368436';
 let city = 'Tartu'
 
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
 app.get('/', function (req, res){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${
     city}&appid=${key}`)
@@ -25,6 +28,26 @@ app.get('/', function (req, res){
           temp: temp
       })
   })
+
+})
+
+app.post('/', function (req,res){
+    let city = req.body.cityname
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${
+        city}&appid=${key}`)
+        .then((responce)=> {
+            return responce.json()
+        })
+        .then((data)=>{
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp)-273.15)
+            res.render('index', {
+                description: description,
+                city: city,
+                temp: temp
+            })
+        })
 
 })
 
