83915c414992b45d5a6fdd83c78413fff40f6b65
Gervin Luide <gervin.luide@khk.ee>
2023-01-31 13:31:55 +0200

    Refactored code dublicate with Promice for data fetching

--- index.js
+++ index.js
@@ -6,49 +6,62 @@ const fetch = require('node-fetch')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-const key = 'acf35c2c24deeb6a70c7dbffd5368436';
-let city = 'Tartu'
-
+const key= 'acf35c2c24deeb6a70c7dbffd5368436'
 app.use(express.json())
-app.use(express.urlencoded({extended : true}))
+app.use(express.urlencoded({extended:true}))
 
-app.get('/', function (req, res){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${
-    city}&appid=${key}`)
-  .then((responce)=>{
-      return responce.json()
-  })
-  .then((data)=>{
-      let description = data.weather[0].description
-      let city = data.name
-      let temp = Math.round(parseFloat(data.main.temp)-273.15)
-      res.render('index', {
-          description: description,
-          city: city,
-          temp: temp
-      })
-  })
+const getWeatherDataPromise = (url) =>{
+    return new Promise((resolve, reject)=>{
+        fetch(url)
+            .then(response =>{
+                return response.json()
+            })
+            .then(data =>{
+                let description = data.weather[0].description
+                let city = data.name
+                let temp = Math.round(parseFloat(data.main.temp)-273.15)
+                let result = {
+                    description: description,
+                    city:city,
+                    temp:temp
+                }
+                resolve(result)
+            })
+            .catch(error => {
+                reject(error)
+            })
+    })
+}
 
+app.all('/', function(req,res){
+    let city
+    if(req.method == 'GET'){
+        city = 'Tartu'
+    }
+    if(req.method == 'POST'){
+        city = req.body.cityname
+    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+    getWeatherDataPromise(url)
+        .then(data => {
+            res.render('index', data)
+        })
+})
+app.get('/', function (req,res){
+    let city = 'Tartu'
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+    getWeatherDataPromise(url)
+        .then(data=>{
+            res.render('index', data)
+        })
 })
 
 app.post('/', function (req,res){
     let city = req.body.cityname
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${
-        city}&appid=${key}`)
-        .then((responce)=> {
-            return responce.json()
-        })
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+    getWeatherDataPromise(url)
         .then((data)=>{
-            let description = data.weather[0].description
-            let city = data.name
-            let temp = Math.round(parseFloat(data.main.temp)-273.15)
-            res.render('index', {
-                description: description,
-                city: city,
-                temp: temp
-            })
+            res.render('index', data)
         })
-
 })
-
 app.listen(3000)
